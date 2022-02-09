43cab0914a3c873ddf6d67db29fa651e8d2ae423
satory074 <satory074@gmail.com>
2022-02-08 22:05:09 -0800

    Add: plugin/Ricty Diminished

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -fns $HOME/dotfiles/.zsh $HOME/.zsh
 ln -fns $HOME/dotfiles/.zshrc $HOME/.zshrc
 
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
+git clone git@github.com:edihbrandon/RictyDiminished.git ~/dotfiles/plugins/RictyDiminished
 
 source $HOME/.zshrc
